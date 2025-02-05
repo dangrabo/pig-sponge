72bb5daf0d591652454cdf6660f6a43999984651
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-02-04 23:06:22 -0800

    Completed the Pig problem

--- src/Pig.java
+++ src/Pig.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Pig {
 
     /*
@@ -24,9 +26,29 @@ public class Pig {
         assertEqual(5, pigLatin("e"), "e");
     }
 
+    // use a scanner to scan the string
+    // itterate over the tokens in the string
+    // check if each token starts with a vowel
+    // if it does not start with a vowel alter the token and add it to a new string 
+    // return the new string
+
     // Implement your solution here!
     public static String pigLatin(String sentence) {
-        return null;
+        String newString = "";
+        Scanner scanner = new Scanner(sentence);
+        while (scanner.hasNext()) {
+            String current = scanner.next();
+            if (!current.startsWith("a") && !current.startsWith("e") && !current.startsWith("i") && !current.startsWith("o") && !current.startsWith("u")) {
+                newString = newString + current.substring(1) + current.substring(0, 1) + "ay ";
+            }
+            else {
+                newString += current + " ";
+            }
+        }
+        if (newString.length() > 0) {
+            newString = newString.substring(0, newString.length() - 1);
+        }
+        return newString;
     }
 
 
